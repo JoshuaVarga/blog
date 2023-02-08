9e73adb1d3d5e79095c4f7e4ec817dea4e972c19
Joshua Varga <joshuasvarga@gmail.com>
2023-02-08 12:46:32 -0500

    testing supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import supabase from "./client";
 
 function App() {
-  return <h1>Hello, World!</h1>;
+  const [countries, setCountries] = useState([]);
+
+  async function getCountries() {
+    const { data } = await supabase.from("countries").select();
+    setCountries(data);
+  }
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  return (
+    <div>
+      {countries.map((country) => (
+        <h1 key={country.id}>{country.name}</h1>
+      ))}
+    </div>
+  );
 }
 
 export default App;
